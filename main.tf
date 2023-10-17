c6283e66336835344c0dc1fc16fddb8b7dd66e17
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-10-17 18:31:36 +0200

    new version

--- main.tf
+++ main.tf
@@ -9,19 +9,19 @@ resource "azurerm_virtual_network" "vnet" {
   subnet {
     name           = var.subnet_name[0]
     address_prefix = var.subnet_address_prefix[0]
-    //security_group = var.security_group
+    security_group = var.security_group
   }
 
   subnet {
     name           = var.subnet_name[1]
     address_prefix = var.subnet_address_prefix[1]
-    //security_group = var.security_group
+    security_group = var.security_group
   }
 
   subnet {
     name           = var.subnet_name[2]
     address_prefix = var.subnet_address_prefix[2]
-    //security_group = var.security_group
+    security_group = var.security_group
   }
 
   tags = var.tags
